425a15ba4f820dd06f2a421e76e0013752006940
EnriqueE <1500000@gmail.com>
2021-12-18 01:35:56 +0100

    Player Controller
    Pre-Glow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float horizontalInput;
-    private float verticalInput;
-
+    
     public float maxAngleRotation = 0.4f;
     public float returnAngleSpeed = 0.05f; 
     public float rotationSpeed;
+    public Vector2 speed = new Vector2(1, 1);
+
+    [Header("Baundaries")]
+    public float boundaryTop = 5.0f;
+    public float boundaryBottom = -3.0f; 
+    public float boundaryHorizontal = 9.0f;
+    [Space(10)]
+
+
+    private float horizontalInput;
+    private float verticalInput;
     private Vector3 rotation;
     private float minRotationYToRestart = 0.0001f; 
 
@@ -19,30 +28,44 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         
+        HandleRotation();
+        HandleMovement(); 
 
+    }
+    private void HandleRotation()
+    {
+        float m_horizontalInput = horizontalInput; 
         if (horizontalInput != 0 || transform.rotation.y != 0)
         {
-            if(horizontalInput == 0)
+            if (horizontalInput == 0)
             {
-                horizontalInput = transform.rotation.y > 0 ? returnAngleSpeed : -returnAngleSpeed;               
+                m_horizontalInput = transform.rotation.y > 0 ? returnAngleSpeed : -returnAngleSpeed;
             }
-            if ((transform.rotation.y < maxAngleRotation && horizontalInput < 0) ||
-                (transform.rotation.y > -maxAngleRotation && horizontalInput > 0))
+            if ((transform.rotation.y < maxAngleRotation && m_horizontalInput < 0) ||
+                (transform.rotation.y > -maxAngleRotation && m_horizontalInput > 0))
             {
-                transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * -horizontalInput);
-                if(transform.rotation.y <= minRotationYToRestart && 
+                transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * -m_horizontalInput);
+                if (transform.rotation.y <= minRotationYToRestart &&
                     transform.rotation.y >= -minRotationYToRestart)
                 {
                     transform.rotation = Quaternion.identity;
                 }
-               
-
-
             }
-
-        }       
-     
+        }
+    }
+    private void HandleMovement()
+    {
         
-
+        if(horizontalInput != 0 || verticalInput != 0) 
+        {
+            transform.Translate(
+                Time.deltaTime * speed.x * horizontalInput, 
+                Time.deltaTime * speed.y * verticalInput, 
+                0,Space.World);
+        }
+        transform.position = new Vector3(
+            Mathf.Max(-boundaryHorizontal, Mathf.Min(boundaryHorizontal,transform.position.x)),
+            Mathf.Max(boundaryBottom, Mathf.Min(boundaryTop, transform.position.y)),
+            0);
     }
 }
